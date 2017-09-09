6780264841110f46187c312e928806a32396f1ae
huangkai <18778335525@163.com>
2017-09-09 08:00:06 +0800

    进行响应的各种尝试

--- FlaskyWeb.py
+++ FlaskyWeb.py
@@ -1,4 +1,8 @@
 from flask import Flask
+# from flask import request
+# from flask import make_response
+# from flask import redirect
+# from flask import abort
 
 app = Flask(__name__)
 
@@ -12,6 +16,32 @@ def index():
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
 
+
+# # 请求上下文
+# @app.route('/')
+# def index():
+#     user_agent = request.headers.get('User-Agent')
+#     return '<p>Your browser is %s</p>' % user_agent
+#
+#
+# # 状态码
+# @app.route('/')
+# def index():
+#     return '<h1>Bad Request</h1>', 400
+#
+#
+# # make_response函数
+# @app.route('/')
+# def index():
+#     response = make_response('<h1>This document carries a cookie!</h1>')
+#     response.set_cookie('answer', 42)
+#     return response
+#
+#
+# # 重定向函数
+# @app.route('/')
+# def index():
+#     return redirect('http://www.example.com')
 
 if __name__ == '__main__':
     app.run()
