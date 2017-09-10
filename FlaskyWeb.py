17d8e1f7c1a3d428de643322b4b9acba62622c79
huangkai <18778335525@163.com>
2017-09-10 08:52:53 +0800

    添加表单

--- FlaskyWeb.py
+++ FlaskyWeb.py
@@ -3,17 +3,27 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 # from flask import request
 # from flask import make_response
 # from flask import redirect
 # from flask import abort
 
 app = Flask(__name__)
+# 设置密钥
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
@@ -30,6 +40,12 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 
 
